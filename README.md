ceb5c066fc66cc88bd4f3e67a777721f5bf35f47
jaeyun95 <wodbs9522@gmail.com>
2019-12-02 01:53:05 +0900

    add algorithm day02

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 알고리즘 기초 공부
 
 문제 및 자세한 설명
-▶blog.naver.com/jaeyoon_95
+▶https://blog.naver.com/jaeyoon_95
 
---
+++ day02.py
@@ -0,0 +1,10 @@
+#(2) day02 좌표평면 두 점 사이 거리 구하기
+
+import math
+
+a=(0,0)
+b=(2,2)
+def lenght(a,b):
+    return math.sqrt(math.pow(b[1]-a[0],2)+math.pow(b[1]-a[0],2))
+	
+
